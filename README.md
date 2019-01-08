77b37317c08e34f2d92cfe1a510bf670c4495a90
mk-pmb <mk-pmb@users.noreply.github.com>
2019-01-08 20:12:13 +0100

    v0.0.1

---
+++ README.md
@@ -0,0 +1,32 @@
+﻿
+<!--#echo json="package.json" key="name" underline="=" -->
+eslint-config-nodejs-pmb
+========================
+<!--/#echo -->
+
+<!--#echo json="package.json" key="description" -->
+My favorite eslint rules
+<!--/#echo -->
+
+
+
+<!--#toc stop="scan" -->
+
+
+
+Known issues
+------------
+
+* Needs more/better tests and docs.
+
+
+
+
+&nbsp;
+
+
+License
+-------
+<!--#echo json="package.json" key=".license" -->
+ISC
+<!--/#echo -->
---
+++ rules.js
@@ -0,0 +1,38 @@
+﻿/* -*- tab-width: 2 -*- */
+'use strict';
+
+const instaffo = require('@instaffogmbh/eslint-config-nodejs/rules');
+
+const rules = {
+  ...instaffo.rules,
+
+  // rules docs: https://github.com/eslint/eslint.github.io/tree/master/docs/rules
+
+  'consistent-return': 'off',
+  'func-names': ['error', 'as-needed'],
+  'function-paren-newline': 'off',
+  'lines-around-directive': 'off',
+  'no-console': 'off',
+  'no-extra-semi': 'off',
+  'no-multiple-empty-lines': 'off',
+  'no-multi-spaces': ['off', { ignoreEOLComments: true }],
+  'object-curly-newline': 'off',
+  'prefer-arrow-callback': 'off',
+  'prefer-template': 'off',
+  'semi': ['error', 'always'],
+  'unicode-bom': 'off',
+
+  'quote-props': ['error', 'consistent-as-needed', {
+    keywords: true,
+    numbers: true,
+  }],
+
+  // rules docs: https://github.com/eslint/eslint.github.io/tree/master/docs/rules
+};
+
+const config = {
+  ...instaffo,
+  rules,
+};
+
+module.exports = config;
